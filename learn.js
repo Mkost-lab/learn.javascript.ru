17dfc4552162659ec402095e28bc97a71e992a8b
Mkost-lab <Ligolaiz92@gmail.com>
2020-10-25 13:40:31 +0300

    first step

---
+++ learn.js
@@ -0,0 +1,16 @@
+//Покажите сообщение с помощью внешнего скрипта
+//alert("Я JavaScript!");
+
+// Работа с переменными
+// let admin, name;
+// name = "Джон";
+// admin = name;
+// alert (admin);
+
+// Придумайте правильные имена
+// let ourPlanet = "Earth";
+// let currentUserName = "Mike"
+
+// Какие буквы (заглавные или строчные) использовать для имён констант?
+//const BIRTHDAY = "18.04.1982";
+//const age = someCode(BIRTHDAY);
